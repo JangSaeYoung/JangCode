cb2880236106965f94d3ca4219b425fe57ed1de8
saeyoung <syoungbb25@gmail.com>
2022-01-11 15:43:16 +0900

    mogakko react project

---
+++ src/Content.js
@@ -0,0 +1,4 @@
+function Content(){
+    return<div>"국내 최초 전통주 술 정기 구독 서비스 쇼핑몰"</div>;
+}
+export default Content;
---
+++ src/Title.js
@@ -0,0 +1,4 @@
+function Title(){
+    return<div>술담화</div>;
+}
+export default Title;
